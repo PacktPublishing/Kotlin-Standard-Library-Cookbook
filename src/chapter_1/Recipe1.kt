881c0522dca9b2c44694fc77ca373a529189952a
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-18 20:54:01 +0200

    Add solution for chapter 1 / recipe 3

--- src/chapter_1/Recipe1.kt
+++ src/chapter_1/Recipe1.kt
@@ -1,5 +1,8 @@
 package chapter_1
 
+/**
+ * Exploring the concept of range expressions to iterate through alphabet characters
+ */
 fun main(vararg args: String) {
     for(letter in 'Z' downTo 'A') print(letter)
 }
---
+++ src/chapter_1/Recipe3.kt
@@ -0,0 +1,41 @@
+package chapter_1
+
+import java.time.LocalDate
+
+/**
+ * Building a custom progression to traverse dates
+ */
+fun main(vararg args: String) {
+    val startDate = LocalDate.of(2020, 1, 1)
+    val endDate = LocalDate.of(2020, 12, 31)
+
+    for (date in startDate..endDate step 7) {
+        println("${date.dayOfWeek} $date ")
+    }
+}
+
+class DateIterator(val startDate: LocalDate,
+                   val endDateInclusive: LocalDate,
+                   val stepDays: Long) : Iterator<LocalDate> {
+    private var currentDate = startDate
+    override fun hasNext() = currentDate <= endDateInclusive
+    override fun next(): LocalDate {
+        val next = currentDate
+        currentDate = currentDate.plusDays(stepDays)
+        return next
+    }
+}
+
+class DateProgression(override val start: LocalDate,
+                      override val endInclusive: LocalDate,
+                      val stepDays: Long = 1) :
+        Iterable<LocalDate>,
+        ClosedRange<LocalDate> {
+    override fun iterator(): Iterator<LocalDate> {
+        return DateIterator(start, endInclusive, stepDays)
+    }
+
+    infix fun step(days: Long) = DateProgression(start, endInclusive, days)
+}
+
+operator fun LocalDate.rangeTo(other: LocalDate) = DateProgression(this, other)
