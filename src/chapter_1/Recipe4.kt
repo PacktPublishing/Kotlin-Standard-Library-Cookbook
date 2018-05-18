1868c6edde0c341d60641b32d2330667ed0e85c1
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-18 21:02:59 +0200

    Add solution for chapter 1 / recipe 4

---
+++ src/chapter_1/Recipe4.kt
@@ -0,0 +1,16 @@
+package chapter_1
+
+import java.util.*
+
+/**
+ * Using range expressions with flow control statements
+ */
+fun main(vararg args: String) {
+    val randomInt = Random().nextInt()
+
+    if (randomInt in 0..10) {
+        print("$randomInt belongs to <0, 10> range")
+    } else {
+        print("$randomInt doesn't belong to <0, 10> range")
+    }
+}
