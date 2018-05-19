166f36b18e06ccf78ecba78d3d2ce172d368d680
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-19 19:02:44 +0200

    Add solution for chapter 5 / recipe 1

--- src/chapter_5/Recipe_1.kt
+++ src/chapter_5/Recipe_1.kt
@@ -1,5 +1,31 @@
 package chapter_5
 
+/**
+ * Chapter: Tasteful design patterns adopting Kotlin concepts
+ * Recipe: Implementing Strategy pattern
+ */
+
 fun main(vararg args: String) {
-    println("Hello World")
+    val lowerCasePrinter = Printer(lowerCaseFormattingStrategy)
+    val upperCasePrinter = Printer(upperCaseFormattingStrategy)
+
+    val text = "This functional-style Strategy pattern looks tasty!"
+
+    lowerCasePrinter.printText(text)
+    upperCasePrinter.printText(text)
+}
+
+class Printer(val textProcessingStrategy: (String) -> String) {
+    fun printText(text: String) {
+        val processedText = textProcessingStrategy(text)
+        println(processedText)
+    }
+}
+
+val lowerCaseFormattingStrategy: (String) -> String = {
+    it.toLowerCase()
+}
+
+val upperCaseFormattingStrategy: (String) -> String = {
+    it.toUpperCase()
 }
