2b22a4c438dd64a085b3438f6651214b6db1aecb
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-25 18:02:56 +0200

    Migrate to gradle build system

---
+++ samples/src/main/kotlin/chapter_5/Recipe_3.kt
@@ -0,0 +1,11 @@
+package chapter_5
+
+import com.google.gson.Gson
+
+/**
+ * Chapter: Tasteful design patterns adopting Kotlin concepts
+ * Recipe: Implementing delegated class properties
+ */
+fun main(vararg args: String) {
+
+}
