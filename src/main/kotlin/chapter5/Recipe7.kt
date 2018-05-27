d6460e7009920fb6e9c03506418e5a57416f5df9
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-27 17:26:14 +0200

    Add placeholder files

---
+++ src/main/kotlin/chapter5/Recipe7.kt
@@ -0,0 +1,11 @@
+package chapter5
+
+/**
+ * Chapter: Tasteful design patterns adopting Kotlin concepts
+ * Recipe: Working with Lazy delegate
+ */
+
+fun main(vararg args: String) {
+
+}
+
---
+++ src/main/kotlin/chapter5/Recipe8.kt
@@ -0,0 +1,11 @@
+package chapter5
+
+/**
+ * Chapter: Tasteful design patterns adopting Kotlin concepts
+ * Recipe: Implementing builders the smart way
+ */
+
+fun main(vararg args: String) {
+
+}
+
