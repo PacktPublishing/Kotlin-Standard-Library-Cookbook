44a2c12100915b7a910e2be0cc3acf995b459709
Samuel Urbanowicz <samiuelson@gmail.com>
2018-05-27 17:25:14 +0200

    Clean up solution for Chapter 5 / Recipe 4

--- src/main/kotlin/chapter5/Recipe4.kt
+++ src/main/kotlin/chapter5/Recipe4.kt
@@ -5,16 +5,17 @@ import kotlin.reflect.KProperty
 
 /**
  * Chapter: Tasteful design patterns adopting Kotlin concepts
- * Recipe: Exploring the power of Delegation pattern
+ * Recipe: Tracking state with Observer pattern
  */
 
 fun main(vararg args: String) {
     val initialTemperature = 1
-    val changesListener = { _: KProperty<*>, oldValue: Int, newValue: Int -> println("Current temperature: $newValue") }
+    val changesListener = { _: KProperty<*>, _: Int, newValue: Int -> println("Current temperature: $newValue") }
     var temperature: Int by Delegates.observable(initialTemperature, changesListener)
 
     temperature = 10
     temperature = 11
     temperature = 12
+    temperature = 30
 }
 
