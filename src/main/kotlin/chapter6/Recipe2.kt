767988aa0789b6c257e6ee7ba888f2fa11e813f8
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-02 20:05:41 +0200

    Implement solution for Chapter 6 / Recipe 2

---
+++ src/main/kotlin/chapter6/Recipe2.kt
@@ -0,0 +1,25 @@
+package chapter6
+
+import java.io.File
+import java.io.File.separator as SEPARATOR
+
+/**
+ * Chapter: Friendly I/O operations
+ * Recipe: Ensuring stream closing with the use function
+ */
+fun main(vararg args: String) {
+
+    val fileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}hello_world.txt"
+
+    val stream1 = File(fileName).inputStream()
+
+    val stream2 = File(fileName).inputStream()
+
+    stream2.use {
+        it.readBytes().also { println(String(it)) }
+    }
+
+//    stream1.readBytes()
+//    stream2.readBytes()
+
+}
