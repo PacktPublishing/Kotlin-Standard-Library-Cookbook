04f4cd59baece1122775b71b6df01f1d2363d04d
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-02 23:43:45 +0200

    Implement solution for Chapter 6 / Recipe 7

---
+++ src/main/kotlin/chapter6/Recipe7.kt
@@ -0,0 +1,20 @@
+package chapter6
+
+import java.io.File
+import java.io.File.separator as SEPARATOR
+
+/**
+ * Chapter: Friendly I/O operations
+ * Recipe: Traversing files in a directory
+ */
+fun main(vararg args: String) {
+
+    val directoryPath = "src${SEPARATOR}main${SEPARATOR}resources"
+
+    val fileTreeWalk: FileTreeWalk = File(directoryPath).walkTopDown()
+    fileTreeWalk
+            .filter { it.isFile }
+            .filter { it.extension == "txt" }
+            .filter { it.readBytes().isNotEmpty() }
+            .forEach { it.readText().apply { println(this) } }
+}
