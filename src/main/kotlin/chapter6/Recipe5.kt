14c5d9c77edf935a66c5fd3016a5141f5740298d
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-02 23:44:27 +0200

    Implement solution for Chapter 6 / Recipe 5

---
+++ src/main/kotlin/chapter6/Recipe5.kt
@@ -0,0 +1,26 @@
+package chapter6
+
+import java.io.File
+import java.io.File.separator as SEPARATOR
+
+/**
+ * Chapter: Friendly I/O operations
+ * Recipe: Appending a file
+ */
+fun main(vararg args: String) {
+
+    val fileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}temp_file"
+
+    val file = File(fileName)
+
+    if (file.exists()) file.delete()
+
+    file.apply {
+        appendText("Yo") // autocloses any streams which are opened during write operation
+        appendText("\n")
+        appendText("Yo")
+        appendText("\n")
+        appendBytes("Yo!".toByteArray())
+    }
+
+}
