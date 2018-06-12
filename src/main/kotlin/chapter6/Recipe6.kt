ef5f0d8806145621b8644ecfe7e360b9f8275f09
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-12 19:25:09 +0200

    Update file name

--- src/main/kotlin/chapter6/Recipe6.kt
+++ src/main/kotlin/chapter6/Recipe6.kt
@@ -9,13 +9,16 @@ import java.io.File.separator as SEPARATOR
  */
 fun main(vararg args: String) {
 
-    val sourceFileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}hello_world.txt"
+    val sourceFileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}file2.txt"
     val sourceFile = File(sourceFileName)
 
-    val targetFileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}hello_world_copy.txt"
+    val targetFileName = "src${SEPARATOR}main${SEPARATOR}resources${SEPARATOR}file2_copy.txt"
     val targetFile = File(targetFileName)
 
     if (targetFile.exists()) targetFile.delete()
 
     sourceFile.copyTo(targetFile)
+
+    File(sourceFileName).readText().apply { println(this) }
+    File(targetFileName).readText().apply { println(this) }
 }
