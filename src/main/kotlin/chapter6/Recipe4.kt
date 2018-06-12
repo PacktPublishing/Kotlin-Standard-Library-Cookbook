bb66c4d9e2f7f43cac68675dbb7843dc4d8cfe72
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-12 19:25:45 +0200

    Update sample text

--- src/main/kotlin/chapter6/Recipe4.kt
+++ src/main/kotlin/chapter6/Recipe4.kt
@@ -17,7 +17,7 @@ fun main(vararg args: String) {
         val text =
                 "\"No one in the brief history of computing " +
                         "has ever written a piece of perfect software. " +
-                        "It's unlikely that you'll be the first.\" - Andy Hunt"
+                        "It's unlikely that you'll be the first.\" \nAndy Hunt"
         writeText(text)
     }
 
