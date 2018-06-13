f8104ac16ce5f314ed83a4bf911d480ad66c504d
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-13 21:21:24 +0200

    Implement solution for chapter 7 / recipe 1

--- src/main/kotlin/chapter7/Recipe1.kt
+++ src/main/kotlin/chapter7/Recipe1.kt
@@ -1,5 +1,33 @@
 package chapter7
 
+import kotlin.concurrent.thread
+
+/**
+ * Chapter: Making Asynchronous programming great again
+ * Recipe: Executing tasks in background using threads
+ */
 fun main(vararg args: String) {
-    println("Hello World")
+    println("Running on ${getCurrentThreadName()}")
+
+    thread {
+        println("Starting async operation on ${getCurrentThreadName()}")
+        `5 sec long task`()
+        println("Ending async operation on ${getCurrentThreadName()}")
+    }
+
+    thread {
+        println("Starting async operation on ${getCurrentThreadName()}")
+        `2 sec long task`()
+        println("Ending async operation on ${getCurrentThreadName()}")
+    }
 }
+
+private fun `5 sec long task`() {
+    Thread.sleep(5000)
+}
+
+private fun `2 sec long task`() {
+    Thread.sleep(2000)
+}
+
+fun getCurrentThreadName(): String = Thread.currentThread().name
