124856bb0d81c644009bcf09285a4a2d8c837c31
Samuel Urbanowicz <samiuelson@gmail.com>
2018-06-22 22:56:09 +0200

    Implement solution for Chapter 7 / Recipe 5

---
+++ src/main/kotlin/chapter7/Recipe5.kt
@@ -0,0 +1,28 @@
+package chapter7
+
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+import kotlin.system.measureTimeMillis
+
+/**
+ * Chapter: Making Asynchronous programming great again
+ * Recipe: Applying coroutines for asynchronous data processing
+ */
+fun main(vararg args: String) = runBlocking {
+    val totalTime = measureTimeMillis {
+        (0..10).mapAsync {
+            delay(1000)
+            it * it
+        }.map { println(it) }
+    }
+
+    println("Total time: $totalTime ms")
+}
+
+suspend fun <T, R> Iterable<T>.mapAsync(transform: suspend (T) -> R) =
+    this.map {
+        async { transform(it) }
+    }.map {
+        it.await()
+    }
