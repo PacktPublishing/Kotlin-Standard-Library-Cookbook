b296e12f7bfc9c1fcf79a99eb745c6baceb9575a
Samuel Urbanowicz <samiuelson@gmail.com>
2018-07-19 10:13:02 +0200

    Implement solution to Chapter 2 / Recipe 1

--- Kotlin-Samples/src/main/kotlin/chapter2/Recipe1.kt
+++ Kotlin-Samples/src/main/kotlin/chapter2/Recipe1.kt
@@ -1,5 +1,15 @@
 package chapter2
 
+/**
+ * Chapter: Expressive functions and adjustable interfaces
+ * Recipe: Declaring adjustable functions with default parameters
+ */
 fun main(vararg args: String) {
-    println("Hello World")
+    val s = calculateDisplacement(acceleration = 9.81f, duration = 1000)
+    println("Displacement of an object in freefall for 1000s is $s m")
 }
+
+private fun calculateDisplacement(initialSpeed: Float = 0f,
+                          acceleration: Float,
+                          duration: Long): Double =
+        initialSpeed * duration + 0.5 * acceleration * duration * duration
