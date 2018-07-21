901033302982b462d9c94aadba4c3f27321a53ab
Samuel Urbanowicz <samiuelson@gmail.com>
2018-07-21 14:21:01 +0200

    Implement solution to Chapter 4 / Recipe 5

--- Kotlin-Samples/src/main/kotlin/chapter4/Recipe5.kt
+++ Kotlin-Samples/src/main/kotlin/chapter4/Recipe5.kt
@@ -1,16 +1,18 @@
 package chapter4
 
 import chapter4.Recipe5.Address
+import chapter4.Recipe5.generateRecipientsString
 
 /**
  * Chapter: Powerful Data Processing
- * Recipe:
+ * Recipe: Building strings based on dataset elements
  */
 fun main(vararg args: String) {
-    fun generateRecipientsString(recipients: List<Address?>): String =
-            recipients.filterNotNull()
-                    .map { it.emailAddress }
-                    .joinToString(", ", "To: ", "<br/>")
+    val addresses = listOf(Address("hilary@gmail.com", "hilly"),
+            Address("bob@gmail.com", "bobby"))
+
+    val text = generateRecipientsString(addresses)
+    println(text)
 }
 
 object Recipe5 {
